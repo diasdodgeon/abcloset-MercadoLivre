ea508669be3e04630597521e702279791702b022
diasdodgeon <145266779+diasdodgeon@users.noreply.github.com>
2025-10-10 15:45:21 -0300

    Add files via upload

---
+++ produtos.js
@@ -0,0 +1,40 @@
+const produtos = [
+      {
+        nome: "Camiseta Oversized Unissex",
+        imagens: ["https://http2.mlstatic.com/D_Q_NP_2X_726034-MLB93632024814_102025-E-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp",
+         "https://http2.mlstatic.com/D_NQ_NP_2X_817618-MLB93632024830_102025-F-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp",
+          "https://http2.mlstatic.com/D_NQ_NP_2X_650262-MLB93632024820_102025-F-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp"
+          ],
+        link: "https://produto.mercadolivre.com.br/MLB-4234402043-bb-2pcs-bra-amamentaco-ultra-maternidade-bra-_JM?has_official_store=false&highlight=false&searchVariation=191419275303&headerTopBrand=false#polycard_client=search-nordic&searchVariation=191419275303&search_layout=grid&position=5&type=item&tracking_id=67dc2e49-e866-4013-9f8c-0bacd47d586e",
+        categoria: "roupas" 
+      },
+
+      {
+        nome: "Calça Cargo Streetwear",
+        imagens: ["https://http2.mlstatic.com/D_Q_NP_2X_726034-MLB93632024814_102025-E-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp",
+         "https://http2.mlstatic.com/D_NQ_NP_2X_817618-MLB93632024830_102025-F-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp",
+          "https://http2.mlstatic.com/D_NQ_NP_2X_650262-MLB93632024820_102025-F-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp"
+          ],
+        link: "https://produto.mercadolivre.com.br/MLB-4234402043-bb-2pcs-bra-amamentaco-ultra-maternidade-bra-_JM?has_official_store=false&highlight=false&searchVariation=191419275303&headerTopBrand=false#polycard_client=search-nordic&searchVariation=191419275303&search_layout=grid&position=5&type=item&tracking_id=67dc2e49-e866-4013-9f8c-0bacd47d586e",
+        categoria: "roupas" 
+      },
+      {
+        nome: "Boné Estilo Urbano",
+        imagens: ["https://http2.mlstatic.com/D_Q_NP_2X_726034-MLB93632024814_102025-E-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp",
+         "https://http2.mlstatic.com/D_NQ_NP_2X_817618-MLB93632024830_102025-F-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp",
+          "https://http2.mlstatic.com/D_NQ_NP_2X_650262-MLB93632024820_102025-F-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp"
+          ],
+        link: "https://produto.mercadolivre.com.br/MLB-4234402043-bb-2pcs-bra-amamentaco-ultra-maternidade-bra-_JM?has_official_store=false&highlight=false&searchVariation=191419275303&headerTopBrand=false#polycard_client=search-nordic&searchVariation=191419275303&search_layout=grid&position=5&type=item&tracking_id=67dc2e49-e866-4013-9f8c-0bacd47d586e",
+        categoria: "roupas" 
+      },
+      {
+        nome: "Jaqueta Jeans Unissex",
+        imagens: ["https://http2.mlstatic.com/D_Q_NP_2X_726034-MLB93632024814_102025-E-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp",
+         "https://http2.mlstatic.com/D_NQ_NP_2X_817618-MLB93632024830_102025-F-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp",
+          "https://http2.mlstatic.com/D_NQ_NP_2X_650262-MLB93632024820_102025-F-bb-2pcs-bra-amamentaco-ultra-maternidade-bra.webp"
+          ],
+        link: "https://produto.mercadolivre.com.br/MLB-4234402043-bb-2pcs-bra-amamentaco-ultra-maternidade-bra-_JM?has_official_store=false&highlight=false&searchVariation=191419275303&headerTopBrand=false#polycard_client=search-nordic&searchVariation=191419275303&search_layout=grid&position=5&type=item&tracking_id=67dc2e49-e866-4013-9f8c-0bacd47d586e",
+        categoria: "roupas" 
+      }
+    ];
+
